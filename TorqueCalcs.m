bc8d15ed40343a4484350a34639cb80575a5d7fd
Dawson Hemphill <34199796+dawsonhemphill@users.noreply.github.com>
2021-11-06 18:01:05 -0400

    Add files via upload

---
+++ TorqueCalcs.m
@@ -0,0 +1,37 @@
+function [TorqueHip,TorqueKnee,TorqueSplay] = TorqueCalcs(HipAngle,KneeAngle,SplayAngle)
+    UpperLegLength = 0.4; %Units in m
+    LowerLegLength = 0.4; %Units in m
+    TotalWeight = 70; %Units in kg
+    Gravity = [0 -9.81 0];
+
+    HipAngle = deg2rad(HipAngle);
+    KneeAngle = deg2rad(KneeAngle);
+    
+    BodyForce = (TotalWeight * Gravity) / 2;
+
+    UpperLegVector = [UpperLegLength*sin(HipAngle),UpperLegLength*cos(HipAngle),0];
+    LowerLegVector = [LowerLegLength*sin(KneeAngle + HipAngle),LowerLegLength*cos(KneeAngle + HipAngle),0];
+
+    TorqueHip = cross((UpperLegVector + LowerLegVector),BodyForce);
+    TorqueKnee = cross(LowerLegVector,BodyForce);
+    TorqueSplay = sin(SplayAngle)* (UpperLegVector(2) + LowerLegVector(2)) * BodyForce;
+
+    TorqueHip   = norm(TorqueHip);
+    TorqueKnee  = norm(TorqueKnee);
+    TorqueSplay = norm(TorqueSplay);
+    
+    % No backwards knees
+    if (KneeAngle+HipAngle < 0)
+        TorqueHip = nan;
+        TorqueKnee = nan;
+    end
+    
+    % No kneeling
+    if (KneeAngle+HipAngle > pi/2)
+        TorqueHip = nan;
+        TorqueKnee = nan;
+    end
+        
+        
+
+end
